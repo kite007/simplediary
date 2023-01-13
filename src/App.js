619aa216f394ee4dee9cea6e25bb51c48536a786
07874 <a07874@DJamil.local>
2023-01-13 12:58:32 +0900

    3rd commit

--- src/App.js
+++ src/App.js
@@ -2,37 +2,54 @@
 import './App.css';
 import DiaryEditor from './DiaryEditor';
 import DiaryList from './DiaryList';
+import { useState, useRef } from 'react';
 
-
-const dummyList = [
-  {
-    id: 1,
-    author:"자밀",
-    content: "하이 1",
-    emotion:5,
-    create_date: new Date().getTime()
-  },
-    {
-    id: 2,
-    author:"홍길동",
-    content: "하이 2",
-    emotion:2,
-    create_date: new Date().getTime()
-  },
-    {
-    id: 3,
-    author:"아무개",
-    content: "하이 3",
-    emotion:1,
-    create_date: new Date().getTime()
-  }
-];
+// const dummyList = [
+//   {
+//     id: 1,
+//     author:"자밀",
+//     content: "하이 1",
+//     emotion:5,
+//     create_date: new Date().getTime()
+//   },
+//     {
+//     id: 2,
+//     author:"홍길동",
+//     content: "하이 2",
+//     emotion:2,
+//     create_date: new Date().getTime()
+//   },
+//     {
+//     id: 3,
+//     author:"아무개",
+//     content: "하이 3",
+//     emotion:1,
+//     create_date: new Date().getTime()
+//   }
+// ];
 
 function App() {
-  return (
+
+ const [data, setData] = useState([]) ;
+ const dataId = useRef(0);
+
+ const onCreate = (author, content, emotioin) => {
+  const create_date = new Date().getTime();
+  const newItem = {
+      author,
+      content,
+      emotioin,
+      create_date,
+      id: dataId.current
+    }
+    dataId.current +=1;
+    setData([newItem, ...data]);
+  };
+
+ return (
     <div className="App">
-      <DiaryEditor/>
-      <DiaryList diaryList={dummyList}/>
+      <DiaryEditor onCreate={onCreate}/>
+      <DiaryList diaryList={data/*dummyList*/}/>
       {/* <DiaryList diaryList={undefined}/> */}
     </div>
   );
--- src/DiaryEditor.js
+++ src/DiaryEditor.js
@@ -1,5 +1,5 @@
 import { useRef, useState } from "react";
-const DiaryEditor = () => {
+const DiaryEditor = ({onCreate}) => {
 
     const authorInput = useRef();
     const contentInput = useRef();
@@ -29,7 +29,13 @@ const DiaryEditor = () => {
             contentInput.current.focus();
             return ;
         }
+        onCreate(state.author, state.content, state.emotion);
         alert("저장 성공")
+        setState({
+            author: "",
+            content: "",
+            emotion:1,
+        });
     };
     // const [author, setAuthor] = useState("");
     // const [content, setContent] = useState("");
